f9541131ad912ab4c6d83ab6d9a059845e0e948a
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2026-01-22 10:32:03 -0300

    Integrate dynamic car data and remote images in model page

--- app/Autos/Modelo/[slug]/page.tsx
+++ app/Autos/Modelo/[slug]/page.tsx
@@ -1,36 +1,66 @@
 import Image from "next/image"
-export default function Page() {
+import { getCar } from "@/app/data/getCar"
+export default async function Page({ params }: { params: { slug: string } }) {
+  const { slug } = await params
+  const car = await getCar({ id: Number(slug) });
+  const cleanDescription = car.description.replace(/<[^>]*>/g, '');
+
   return (
     <>
       <div className="px-3 md:px-0">
         <section className="container mx-auto m-6 p-6">
-          <div className="items-center justify-center grid grid-cols-1 md:grid-cols-2 grid-rows-1 gap-10 m-6 p-6">
-            <Image src="/IMG.png" width={559} height={320} alt="Modelo" />
+          <div className="items-center justify-center grid grid-cols-1 md:grid-cols-2 grid-rows-1 gap-10 md:m-6 md:p-6">
+            <Image 
+              src={car.photo || car.thumbnail} 
+              width={559} 
+              height={320} 
+              alt="Modelo"
+              quality={90}
+              priority
+              className="rounded-lg object-cover"
+              sizes="(max-width: 768px) 100vw, 50vw"
+            />
             <div>
-              <h4 className="font-semibold pb-3 text-[20px]">Toyota Corolla</h4>
-              <h3 className="text-7xl font-bold pb-5">Preparada para cualquier desafío </h3>
-              <p className="text-[18px]/[21px] py-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <h4 className="font-semibold pb-3 text-[20px]">{car.name}</h4>
+              <h3 className="text-6xl font-bold pb-5 text-balance">{car.title}</h3>
+              <p className="text-[18px]/[21px] py-5">{cleanDescription}</p>
             </div>
           </div>
         </section>
         <section className="bg-[#F7F7F7]">
-          <p>carrousel</p>
+          {/* <Carrousel/> */}
         </section>
         <section className="container mx-auto m-6 p-6">
-          <div className="items-center justify-center grid grid-cols-1 md:grid-cols-2 grid-rows-1 gap-10 m-6 p-6">
-            <div className="order-2 md:order-1 text-balance">
-              <h4 className="font-semibold pb-3 text-[20px]">Titulo 20px</h4>
-              <p className="text-[16px]/[27px] py-5">Texto lorem ipsum dolor sit amet orem ipsum dolor sit amet. lorem ipsum dolor sit amet orem ipsum dolor sit amet lorem ipsum dolor sit amet orem ipsum dolor sit amet. AL dorsd</p>
+          <div className="items-center justify-center grid grid-cols-1 md:grid-cols-2 grid-rows-1 gap-10 md:m-6 md:p-6">
+            <div className="ml-0 md:ml-10 order-2 md:order-1 text-balance">
+              <h4 className="font-semibold pb-3 text-[20px]">{car.model_features[0].name}</h4>
+              <p className="text-[16px]/[27px] py-5">{car.model_features[0].description}</p>
             </div>
-            <Image className="order-1 md:order-2" src="/IMG.png" width={559} height={320} alt="Modelo" />
+            <Image 
+              className="order-1 md:order-2 rounded-lg shadow-lg object-cover" 
+              src={car.model_features[0].image} 
+              width={500} 
+              height={500} 
+              alt="Modelo"
+              quality={90}
+              sizes="(max-width: 768px) 100vw, 50vw"
+            />
           </div>
         </section>
         <section className="container mx-auto m-6 p-6">
-          <div className="items-center justify-center grid grid-cols-1 md:grid-cols-2 grid-rows-1 gap-10 m-6 p-6">
-            <Image src="/IMG.png" width={559} height={320} alt="Modelo" />
+          <div className="items-center justify-center grid grid-cols-1 md:grid-cols-2 grid-rows-1 gap-10 md:m-6 md:p-6">
+            <Image 
+              src={car.model_features[1].image} 
+              width={500} 
+              height={500} 
+              alt="Modelo"
+              quality={90}
+              className="rounded-lg shadow-lg object-cover"
+              sizes="(max-width: 768px) 100vw, 50vw"
+            />
             <div className="text-balance">
-              <h4 className="font-semibold pb-3 text-[20px]">Titulo 20px</h4>
-              <p className="text-[16px]/[27px] py-5">Texto lorem ipsum dolor sit amet orem ipsum dolor sit amet. lorem ipsum dolor sit amet orem ipsum dolor sit amet lorem ipsum dolor sit amet orem ipsum dolor sit amet.</p>
+              <h4 className="font-semibold pb-3 text-[20px]">{car.model_features[1].name}</h4>
+              <p className="text-[16px]/[27px] py-5">{car.model_features[1].description}</p>
             </div>
           </div>
         </section>
